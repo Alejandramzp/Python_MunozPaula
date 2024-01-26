ea2362ca3339985e41189b24544cbfb442950f4d
Paula Muñoz <alejandrapmp17@gmail.com>
2024-01-26 18:26:12 -0500

    comentarios

--- Dia 2/Ejercicio1_MunozPaula.py
+++ Dia 2/Ejercicio1_MunozPaula.py
@@ -13,40 +13,40 @@ def fibonacci(n):        ## Definir la funciòn y agragarle el paràmetro de n n
     return fibo                          ## Retornar el array
 
 print("-------Bienvenido a la serrie de Fibonacci-------")
-print("La serie comienza con 0 y 1, y cada término siguiente es la suma de los dos anteriores.")
+print("La serie comienza con 0 y 1, y cada valor siguiente es la suma de los dos valores anteriores.")
 print("----¡Recuerda que ingresas a la serie digitando un nùmero entero positivo y sales digitando 0!----")
 
 
-rta = True
-while  rta:
-    try:
-        n= int(input("Por favor digita la cantidad de nùmeros enteros que deseas en la serie de Fibonacci o por el contrario si deseas salir del programa: "))
-        while n <0:
-             print("¡Error! Digita un nùmero entero positivo")
-             n= int(input("numero"))
-    except ValueError:
-        print("caracter invalido")
-    else:
-        if n ==0:
-         print("Saliste del programa con èxito")
-         break
-        else:
-            if n >0:
-                rta= False
+rta = True    ##  Nombrar una variable para que cumpla un booleano
+while  rta:    ## Mientras la variable sea verdadera seguira en el while
+    try:          ## Pedir el nùmero de la serie
+        n = int(input("Por favor digita la cantidad de nùmeros enteros que deseas en la serie de Fibonacci o por el contrario si deseas salir del programa: "))
+        while n <0:      # Condiciòn para que el while siga pidiendo un nùmero correcto 
+             print("¡Error! Digita un nùmero entero positivo") ## Mensaje de error si la condiciòn se cumple
+             n= int(input("nùmero serie de Fibonacci: ")) ## Se vuelve a pedir n hasta qye sea mayor de 0
+
+        if n ==0: ## Condiciòn para que salga del programa
+                print("Saliste del programa con èxito")
+                break
+        else: 
+                if n >0:  ##Condiciòn para que la variable booleano se convierta en falsa
+                    rta= False
+    except ValueError:  ## Para que no se pueda digitar valores diferentes a enteros positivos
+        print("¡Error! Valor no vàlido")
+   
 
 
-while n!=0:
+while n!=0:       ## Mientras n sea mayor que 0 se muestra la serie
     print(fibonacci(n))
-    while True:
+    while True:     ## While para que el programa se detenga cuando muestra la lista
             try:
                 n = int(input("Por favor digita la cantidad de nùmeros enteros que deseas en la serie de Fibonacci o por el contrario si deseas salir del programa: ")) 
                 if n < 0 :
                     print("¡Error! Digita un nùmero entero positivo")
                 else:
-                    print("Saliste del programa con èxito")
-                    break
+                     break       
             except ValueError:
                 print("¡Error! Valor no vàlido")
-
+print("Saliste del programa con èxito")  
 
 ## Desarrollado por PAULA ALEJANDRA MUÑOZ PEÑARANDA - 1095953057
