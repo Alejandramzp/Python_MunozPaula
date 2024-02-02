e532291ec0ac5e1e33f1a35e5ca041ff44d7bd4f
Paula Muñoz <alejandrapmp17@gmail.com>
2024-02-02 07:06:36 -0500

    Ejercicio3

--- Dia 6/Ejercicio3_MuñozPaula.py
+++ Dia 6/Ejercicio3_MuñozPaula.py
@@ -1,20 +1,43 @@
 ## ---------------------------------------------------
-## ------------------- Ejercicio 1 -------------------
+## ------------------- Ejercicio 3 -------------------
 ## ---------------------------------------------------
 
-entrada = input("")
-coordenadas = entrada.split(",")
+import math
 
-x = float(coordenadas[0].strip())
-y = float(coordenadas[1].strip())
-r = float(coordenadas[2].strip())
-tupla = {x,y,r}
-print(tupla)
+def ball_collide():
+    if distance<=radio_suma:
+        return True
+    else:
+        return False
 
-print("")
+## coordenadas A
+print("Ingresa las coordenadas y el radio de la bola A") 
+coordinates =input("").split(" ")
 
+p = coordinates
+x1 = float(p[0])
+y1 = float(p[1])
+r1 = float(p[2])
+coord1 = (x1,y1,r1)
+listA =[coord1]
 
+## coordenadas B
+print("Ingresa las coordenadas y el radio de la bola A") 
+coordinates =input("").split(" ")
 
+q = coordinates
+x2 = float(p[0])
+y2 = float(p[1])
+r2 = float(p[2])
+coord2 = (x2,y2,r2)
+listB =[coord2]
 
+print("Coordenadas y el radio de la bola A son:",listA)
+print("Coordenadas y el radio de la bola B son:",listB)
+
+distance = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+radio_suma = r1 + r2
+
+print(ball_collide())
 
 ## Creado por PAULA ALEJANDRA MUÑOZ PEÑARANDA
